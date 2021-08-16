08e106afe0dfcf454ea5dd0b0e23ffa2126778a8
Yvan Sabay <sabayyvan2018@gmail.com>
2021-08-16 17:18:56 +0800

    added port

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+app.config['MYSQL_PORT'] = 3306
 
 mysql = MySQL(app)
 
