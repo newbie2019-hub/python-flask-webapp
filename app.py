3e96b6d7664a0ce87350ef5e21e79bb3e3dc0364
Yvan Sabay <sabayyvan2018@gmail.com>
2021-08-16 16:18:19 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect, flash
+from flask_mysqldb import MySQL
+import yaml
+
+app = Flask(__name__)
+app.secret_key = "Python-WebApp-CRUD"
+
+# Configure db
+db = yaml.load(open('config.yaml'), Loader=yaml.FullLoader)
+app.config['MYSQL_HOST'] = db['MYSQL_HOST']
+app.config['MYSQL_USER'] = db['MYSQL_USER']
+app.config['MYSQL_PASSWORD'] = db['MYSQL_PASSWORD']
+app.config['MYSQL_DB'] = db['MYSQL_DB']
+
+mysql = MySQL(app)
+
+@app.route('/')
+def Index():
+    cur = mysql.connection.cursor()
+    resultValue = cur.execute("SELECT * FROM fruits")
+    if resultValue > 0:
+        fruitData = cur.fetchall()
+        return render_template("index.html", fruits = fruitData)
+    else:
+        fruitData = ''
+        return render_template("index.html", fruits = fruitData)
+
+#this route is for inserting data to mysql database via html forms
+@app.route('/fruit', methods = ['POST'])
+def insert():
+    if request.method == 'POST':
+        fruit = request.form['fruit']
+        quantity = request.form['qty']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO fruits(fruit_name, quantity) VALUES(%s, %s)",(fruit, quantity))
+        mysql.connection.commit()
+        cur.close()
+        flash("Fruit saved successfully")
+        return redirect('/')
+
+#this route is for inserting data to mysql database via html forms
+@app.route('/fruit/<id>/', methods = ['POST', 'GET'])
+def delete(id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM fruits WHERE id = %s",(id))
+    mysql.connection.commit()
+    cur.close()
+    flash("Fruit deleted successfully")
+    return redirect('/')
+
+@app.route('/update', methods = ['GET', 'POST'])
+def update():
+    fruit = request.form['fruit']
+    quantity = request.form['qty']
+    id = request.form['id']
+    cur = mysql.connection.cursor()
+    cur.execute("UPDATE fruits SET fruit_name=%s, quantity=%s WHERE id = %s",(fruit, quantity, id))
+    mysql.connection.commit()
+    cur.close()
+    flash("Fruit updated successfully")
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
